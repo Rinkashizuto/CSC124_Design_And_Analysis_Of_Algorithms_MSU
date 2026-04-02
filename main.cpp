520424781156c3be60950d87213b51b7975e29cd
Rinkashizuto <marjaantaher@gmail.com>
2026-04-02 16:44:08 +0800

    Implemented File Graph
    This version can now handle files. Take user input and store in file. AND be able to read that file and  use it in the program as well. ALSO FIXED MERGE ISSUE

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main(){
     int start = 0;
     std::string fileName = "test";
 
-    // int edges[][3] = {{0, 1, 10}, {1, 2, 5}, {2, 3, 8},
-    //                   {3, 0, 7},  {0, 4, 3}, {1, 4, 12}};
-    // for(int i = 0; i < v; i++){
-    //     g.addEdges(edges[i][0], edges[i][1], edges[i][2]);
-    // }
+    int edges[][3] = {{0, 1, 10}, {1, 2, 5}, {2, 3, 8},
+                      {3, 0, 7},  {0, 4, 3}, {1, 4, 12}};
+    for(int i = 0; i < v; i++){
+        g.addEdges(edges[i][0], edges[i][1], edges[i][2]);
+    }
 
     g.loadToFile(fileName);
 
